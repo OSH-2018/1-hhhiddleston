4e66c69cb29fc8d8415cda216ca9a49e0132de8c
Ziwei Wu <ziweiwu@Little-Fairys-RMBP.local>
2018-07-04 16:35:57 +0800

    add run.sh

---
+++ lab1/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+gdb ./vmlinux  -ex "target remote localhost:1234"       \
+               -ex "break start_kernel"          \
+               -ex "continue"                           \
+               -ex "disconnect"                         \
+               -ex "set architecture i386:x86-64:intel" \
+               -ex "target remote localhost:1234"
